a80da10c5894d2c9cdef097b1cfa23e14f8524b7
Berserker1523 <berserker1523@hotmail.com>
2019-10-13 15:03:53 -0500

    imports en el server

--- app/server/main.js
+++ app/server/main.js
@@ -1,4 +1,10 @@
 import { Meteor } from "meteor/meteor";
+import "../imports/api/candidatos.js";
+import "../imports/api/propuestas.js";
+import "../imports/api/reacciones.js";
+import "../imports/api/comentarios.js";
+import "../imports/api/usuarios.js";
+
 import "../imports/api/tasks.js";
 
 Meteor.startup(() => {});
