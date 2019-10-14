0dfda43893177409e3a062ea98464d36fdb9073a
Maria Ocampo <mj.ocampov@uniandes.edu.co>
2019-10-13 22:51:35 -0500

    home finish

---
+++ app/imports/ui/CandidatoPerfil.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "./Navbar.jsx";
+import api from "../api/candidatos.json.js";
+
+class CandidatoPerfil extends React.Component {
+
+}
+
+export default CandidatoPerfil;
