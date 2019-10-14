0ffb8ba43e91795af152f2def167cec70b90f6b6
Berserker1523 <berserker1523@hotmail.com>
2019-10-14 08:04:37 -0500

    ni idea de que cambios hice

--- app/imports/ui/Inicio.jsx
+++ app/imports/ui/Inicio.jsx
@@ -3,55 +3,53 @@ import Navbar from "./Navbar.jsx";
 import api from "../api/candidatos.json.js";
 
 class Inicio extends React.Component {
-
   renderCandidatos() {
-    return
+    return;
   }
   render() {
-  return (
-    <div>
-      <Navbar paginaActual="Inicio" />
-      <div className="container-fluid inicio">
-        <div className="candidatos">
-          <h1>Conoce los candidatos</h1>
+    return (
+      <div>
+        <Navbar paginaActual="Inicio" />
+        <div className="container-fluid inicio">
+          <div className="candidatos">
+            <h1>Conoce los candidatos</h1>
             <div className="container-fluid">
               <div className="row">
-              {api.candidatos.map(candidato => (
-                <div className="col-sm" key={candidato.id}>
-                  <img
-                    className="img-candidato"
-                    src={candidato.foto_url}
-                    alt={`Candidato ${candidato.nombre}`}
-                  />
-                  <h2 className="nombre-candidato">{candidato.nombre}</h2>
-                </div>
+                {api.candidatos.map(candidato => (
+                  <div className="col-sm" key={candidato.id}>
+                    <img
+                      className="img-candidato"
+                      src={candidato.foto_url}
+                      alt={`Candidato ${candidato.nombre}`}
+                    />
+                    <h2 className="nombre-candidato">{candidato.nombre}</h2>
+                  </div>
                 ))}
               </div>
             </div>
-        </div>
-        <div className="instrucciones">
-          <h1>Arma tu propio plan de gobierno</h1>
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-sm">
-                <h1 className="paso">Paso 1</h1>
-
-              </div>
-              <div className="col-sm">
-                <h1 className="paso">Paso 2</h1>
-              </div>
-              <div className="col-sm">
-                <h1 className="paso">Paso 3</h1>
+          </div>
+          <div className="instrucciones">
+            <h1>Arma tu propio plan de gobierno</h1>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-sm">
+                  <h1 className="paso">Paso 1</h1>
+                </div>
+                <div className="col-sm">
+                  <h1 className="paso">Paso 2</h1>
+                </div>
+                <div className="col-sm">
+                  <h1 className="paso">Paso 3</h1>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     );
   }
 }
-  /*const mostrarCandidatos = () => {
+/*const mostrarCandidatos = () => {
     console.log("Llgeue aki");
     console.log("candidatos: ");
     console.log(api.candidatos);
